1d41c83ee1b4965d8f2c941244056ff1f9c5d407
Grisw <514335188@qq.com>
2017-06-04 22:58:22 +0800

    添加Demo_InteractObject

---
+++ Assets/Script/Demo_Sphere.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class Demo_Sphere : VRTK_InteractableObject {
+
+    public override void StartUsing(GameObject currentUsingObject) {
+        base.StartUsing(currentUsingObject);
+        Debug.Log("Using Object!");
+    }
+
+    public override void StopUsing(GameObject currentUsingObject) {
+        base.StopUsing(currentUsingObject);
+        Debug.Log("Stop Using Object!");
+    }
+}
