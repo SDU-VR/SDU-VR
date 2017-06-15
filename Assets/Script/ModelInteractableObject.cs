8e5d27b50811effcde518e3dcf9fc7cb7f6d9c52
Grisw <514335188@qq.com>
2017-06-15 19:25:27 +0800

    添加控制脚本。

---
+++ Assets/Script/ModelInteractableObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class ModelInteractableObject : VRTK_InteractableObject {
+
+    private enum State { IDLE, MOVING, PLACED }
+
+    public override void StartUsing(GameObject currentUsingObject) {
+        base.StartUsing(currentUsingObject);
+        Debug.Log("using");
+    }
+
+}
