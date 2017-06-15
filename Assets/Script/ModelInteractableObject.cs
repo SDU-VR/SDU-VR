91f15819c724fa18eba205d6c0d3b6b34b42f380
Grisw <514335188@qq.com>
2017-06-15 23:33:57 +0800

    单层交互。

--- Assets/Script/ModelInteractableObject.cs
+++ Assets/Script/ModelInteractableObject.cs
@@ -4,8 +4,30 @@ using VRTK;
 
 public class ModelInteractableObject : VRTK_InteractableObject {
 
-    private enum State { IDLE, MOVING, PLACED }
-    private State state = State.IDLE;
+	[Header("Model Interactable Object")]
+
+	[Tooltip("移动面前的距离")]
+	public float Distance = 3f;
+	[Tooltip("移动面前的高度")]
+	public float Height = 1f;
+	[Tooltip("移动到目标位置的时间，以帧为单位")]
+	public float SpeedFrame = 30.0f;
+	[Tooltip("Pivot差值")]
+	public float Pivot = 0;
+
+	private enum State { IDLE, MOVING, PLACED }
+
+	private State state;
+	private Vector3 originPosition;
+	private Vector3 pivot;
+	private Vector3 scaleDelta;
+
+	void Start(){
+		state = State.IDLE;
+		originPosition = transform.position;
+		pivot = new Vector3 (0, Pivot, 0);
+		scaleDelta = new Vector3 (0.5f, 0.5f, 0.5f) / SpeedFrame;
+	}
 
     public override void StartUsing(GameObject currentUsingObject) {
         base.StartUsing(currentUsingObject);
@@ -16,26 +38,37 @@ public class ModelInteractableObject : VRTK_InteractableObject {
                 break;
             case State.MOVING:
                 break;
-            case State.PLACED:
+		case State.PLACED: {
+				StartCoroutine(moveBack());
+				}
                 break;
         }
     }
 
     private IEnumerator moveToPlayArea() {
         state = State.MOVING;
-        Vector3 playArea = VRTK_DeviceFinder.PlayAreaTransform().position;
-        Vector3 dir = playArea - transform.position;
-        Vector3 horizon = new Vector3(dir.x, 0, dir.y);
-        for(int i = 0; i < 100; i++) {
-            transform.Translate(horizon * 0.01f);
-            yield return new WaitForEndOfFrame();
-        }
-        Vector3 vertical = new Vector3(0, dir.y, 0);
-        for (int i = 0; i < 40; i++) {
-            transform.Translate(vertical * 0.025f);
+		Transform headset = VRTK_DeviceFinder.HeadsetTransform();
+		Vector3 position = headset.position;
+		position.y = Height;
+		position += new Vector3(headset.forward.x, 0, headset.forward.z) * Distance;
+		Vector3 dirDelta = (position - (transform.position + pivot)) / SpeedFrame;
+		for(int i = 0; i < SpeedFrame; i++) {
+			transform.Translate (dirDelta , Space.World);
+			transform.localScale -= scaleDelta;
             yield return new WaitForEndOfFrame();
         }
         state = State.PLACED;
     }
+
+	private IEnumerator moveBack() {
+		state = State.MOVING;
+		Vector3 dirDelta = (originPosition - transform.position) / SpeedFrame;
+		for(int i = 0; i < SpeedFrame; i++) {
+			transform.Translate (dirDelta , Space.World);
+			transform.localScale += scaleDelta;
+			yield return new WaitForEndOfFrame();
+		}
+		state = State.IDLE;
+	}
 
 }
